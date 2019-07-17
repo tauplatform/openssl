7a89e6ab836d290d62f4daeb5a9e89f4778b22e5
Dmitry Soldatenkov <dsoldatenkov@tau-technologies.com>
2019-07-17 03:52:09 +0300

    update for build additional architectures for iOS

--- tau_build_universal_ios_libs.sh
+++ tau_build_universal_ios_libs.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 export CC=clang
 export CROSS_TOP=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer
-export CROSS_SDK=iPhoneOS11.3.sdk
+export CROSS_SDK=iPhoneOS12.1.sdk
 
 make clean
-./Configure ios64-cross enable-ssl2 enable-ssl3 no-engine no-hw no-comp -DOPENSSL_SMALL_FOOTPRINT -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_JPAKE -DOPENSSL_NO_DYNAMIC_ENGINE -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_ASYNC --prefix=/usr/local/openssl64-ios
+./Configure ios64e-cross enable-ssl2 enable-ssl3 no-engine no-hw no-comp -DOPENSSL_SMALL_FOOTPRINT -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_JPAKE -DOPENSSL_NO_DYNAMIC_ENGINE -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_ASYNC --prefix=/usr/local/openssl64-ios
 make depend && make all
 mkdir ./libopenssl_ios
 cd ./libopenssl_ios
 rm -rf *.a
-cp ../libcrypto.a libcryptoARM64.a
-cp ../libssl.a libsslARM64.a
+cp ../libcrypto.a libcrypto_arm64e.a
+cp ../libssl.a libssl_arm64e.a
+cd ../
+
+make clean
+./Configure ios64-cross enable-ssl2 enable-ssl3 no-engine no-hw no-comp -DOPENSSL_SMALL_FOOTPRINT -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_JPAKE -DOPENSSL_NO_DYNAMIC_ENGINE -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_ASYNC --prefix=/usr/local/openssl64-ios
+make depend && make all
+cd ./libopenssl_ios
+cp ../libcrypto.a libcrypto_arm64.a
+cp ../libssl.a libssl_arm64.a
 cd ../
 
 
@@ -18,13 +26,22 @@ make clean
 ./Configure ios-cross enable-ssl2 enable-ssl3 no-engine no-hw no-comp -DOPENSSL_SMALL_FOOTPRINT -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_JPAKE -DOPENSSL_NO_DYNAMIC_ENGINE -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_ASYNC --prefix=/usr/local/openssl-ios
 make depend && make all
 cd ./libopenssl_ios
-cp ../libcrypto.a libcryptoARM.a
-cp ../libssl.a libsslARM.a
+cp ../libcrypto.a libcrypto_armv7.a
+cp ../libssl.a libssl_armv7.a
 cd ../
+
+make clean
+./Configure ios-cross7s enable-ssl2 enable-ssl3 no-engine no-hw no-comp -DOPENSSL_SMALL_FOOTPRINT -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_JPAKE -DOPENSSL_NO_DYNAMIC_ENGINE -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_ASYNC --prefix=/usr/local/openssl-ios
+make depend && make all
+cd ./libopenssl_ios
+cp ../libcrypto.a libcrypto_armv7s.a
+cp ../libssl.a libssl_armv7s.a
+cd ../
+
 
 export CC=clang
 export CROSS_TOP=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer
-export CROSS_SDK=iPhoneSimulator11.3.sdk
+export CROSS_SDK=iPhoneSimulator12.1.sdk
 
 make clean
 ./Configure iphoneos-cross enable-ssl2 enable-ssl3 no-shared no-engine no-hw no-comp -DOPENSSL_SMALL_FOOTPRINT -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_JPAKE -DOPENSSL_NO_DYNAMIC_ENGINE -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_ASYNC --prefix=/usr/local/openssl-iphoneos
@@ -36,7 +53,7 @@ cd ../
 
 export "CC=/usr/bin/gcc -arch i386"
 export CROSS_TOP=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer
-export CROSS_SDK=iPhoneSimulator11.3.sdk
+export CROSS_SDK=iPhoneSimulator12.1.sdk
 
 make clean
 ./Configure iphoneos-cross enable-ssl2 enable-ssl3 no-shared no-engine no-hw no-comp -DOPENSSL_SMALL_FOOTPRINT -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_JPAKE -DOPENSSL_NO_DYNAMIC_ENGINE -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_ASYNC --prefix=/usr/local/openssl-iphoneos-cross386
@@ -47,5 +64,5 @@ cp ../libssl.a libsslx86.a
 
 
 
-lipo -create libcryptoARM.a libcryptoARM64.a libcryptox86_64.a libcryptox86.a -output libcrypto.a
-lipo -create libsslARM.a libsslARM64.a libsslx86_64.a libsslx86.a -output libssl.a
+lipo -create libcrypto_armv7.a libcrypto_armv7s.a libcrypto_arm64.a libcrypto_arm64e.a libcryptox86_64.a libcryptox86.a -output libcrypto.a
+lipo -create libssl_armv7.a libssl_armv7s.a libssl_arm64.a libssl_arm64e.a libsslx86_64.a libsslx86.a -output libssl.a
