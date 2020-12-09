54f9d6252ac0b51387ab4f2bc4c73c0818e3a4d7
Dmitry Soldatenkov <dsoldatenkov@tau-technologies.com>
2020-12-09 21:09:49 +0300

    iOS: add bitcode to builded binary

--- tau_build_universal_ios_libs.sh
+++ tau_build_universal_ios_libs.sh
@@ -4,13 +4,13 @@ export CROSS_TOP=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.p
 export CROSS_SDK=iPhoneOS14.1.sdk
 
 make clean
-./Configure ios64e-cross enable-ssl2 enable-ssl3 no-engine no-hw no-comp -DOPENSSL_SMALL_FOOTPRINT -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_JPAKE -DOPENSSL_NO_DYNAMIC_ENGINE -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_ASYNC --prefix=/usr/local/openssl64-ios
+./Configure ios-cross enable-ssl2 enable-ssl3 no-engine no-hw no-comp -DOPENSSL_SMALL_FOOTPRINT -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_JPAKE -DOPENSSL_NO_DYNAMIC_ENGINE -DOPENSSL_NO_RC5 -DOPENSSL_NO_MD2 -DOPENSSL_NO_KRB5 -DOPENSSL_NO_ASYNC --prefix=/usr/local/openssl-ios
 make depend && make all
 mkdir ./libopenssl_ios
 cd ./libopenssl_ios
 rm -rf *.a
-cp ../libcrypto.a libcrypto_arm64e.a
-cp ../libssl.a libssl_arm64e.a
+cp ../libcrypto.a libcrypto_armv7.a
+cp ../libssl.a libssl_armv7.a
 cd ../
 
 make clean
@@ -49,7 +49,7 @@ cp ../libssl.a libssl_arm64sim.a
 
 #lipo -create libcrypto_arm64.a libcrypto_arm64e.a libcryptox86_64.a libcrypto_arm64sim.a -output libcrypto.a
 #lipo -create libssl_arm64.a libssl_arm64e.a libsslx86_64.a libssl_arm64sim.a -output libssl.a
-lipo -create libcrypto_arm64.a libcrypto_arm64e.a -output libcrypto.a
+lipo -create libcrypto_arm64.a libcrypto_armv7.a -output libcrypto.a
 lipo -create libcryptox86_64.a libcrypto_arm64sim.a -output libcrypto_sim.a
-lipo -create libssl_arm64.a libssl_arm64e.a -output libssl.a
+lipo -create libssl_arm64.a libssl_armv7.a -output libssl.a
 lipo -create libsslx86_64.a libssl_arm64sim.a -output libssl_sim.a
